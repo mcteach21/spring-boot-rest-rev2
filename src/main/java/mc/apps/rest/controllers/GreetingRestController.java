1fae512880d26a0fd2f3d3ab6bcf84543f6f7d69
MC-Teacher <mc.teach.21@gmail.com>
2022-02-07 16:13:45 +0100

    Spring boot - hateoas greeting controller

---
+++ src/main/java/mc/apps/rest/controllers/GreetingRestController.java
@@ -0,0 +1,27 @@
+package mc.apps.rest.controllers;
+
+import mc.apps.rest.modele.Greeting;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.hateoas.server.core.DummyInvocationUtils.methodOn;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+@RestController
+public class GreetingRestController {
+
+
+    @GetMapping("/rest/greeting")
+    public HttpEntity<Greeting> getGreeting(@RequestParam(value = "name", defaultValue = "World") String name){
+
+        Greeting greeting = new Greeting(String.format("Hello %s!",name));
+
+        greeting.add(linkTo(methodOn(GreetingRestController.class).getGreeting(name)).withSelfRel());
+
+        return new ResponseEntity<>(greeting, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/mc/apps/rest/modele/Greeting.java
@@ -0,0 +1,21 @@
+package mc.apps.rest.modele;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.hateoas.RepresentationModel;
+
+public class Greeting extends RepresentationModel<Greeting> {
+    /**
+     * Spring HATEOAS class RepresentationModel => add instances of Link
+     */
+    private final String content;
+
+    @JsonCreator
+    public Greeting(@JsonProperty("content") String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
